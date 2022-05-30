32313647c80be0fc782f38c6b5588aea99bc1f58
abuAlEzzAlMushkali <thankyouplatformteam@gmail.com>
2022-05-31 01:29:02 +0300

    Publishers problem solving

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(1)->create();
+        $this->call([CategorySeeder::class]);
+        $this->call([PublisherSeeder::class]);
+        $this->call([AuthorSeeder::class]);
+        $this->call([BookSeeder::class]);
+        $this->call([BookAuthorSeeder::class]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +23,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//
+Route::get('/', [GalleryController::class, 'index'])->name('gallery.index');
+//
+Route::get('/search', [GalleryController::class, 'search'])->name('search');
+//
+Route::get('book/{book}', [BookController::class, 'details'])->name('book.details');
+//
+Route::get('/categories', [CategoryController::class, 'list'])->name('gallery.categories.index');
+Route::get('/categories/search', [CategoryController::class, 'search'])->name('gallery.categories.search');
+Route::get('/categories/{category}', [CategoryController::class, 'result'])->name('gallery.categories.show');
+//
+Route::get('/authors', [AuthorController::class, 'list'])->name('gallery.authors.index');
+Route::get('/authors/search', [AuthorController::class, 'search'])->name('gallery.authors.search');
+Route::get('/authors/{author}', [AuthorController::class, 'result'])->name('gallery.authors.show');
+//
+Route::get('/publishers', [PublisherController::class, 'list'])->name('gallery.publishers.index');
+Route::get('/publishers/search', [PublisherController::class, 'search'])->name('gallery.publishers.search');
+Route::get('/publishers/{publishers}', [PublisherController::class, 'result'])->name('gallery.publishers.show');
+//
+Route::group(['prefix'=>'admin'], function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+    //
+    Route::resource('books',BookController::class);
+    Route::resource('authors',AuthorController::class);
+    Route::resource('categories',CategoryController::class);
+    Route::resource('publishers',PublisherController::class);
+    Route::resource('users',UserController::class);
 });
